ef72a064a334dcc7e838d986db72c2f541474ab7
Pfefferdorn <121354779+Pfefferdorn@users.noreply.github.com>
2023-07-14 16:24:46 +0200

    Update wsgi.py

--- The_Weather/The_Weather/wsgi.py
+++ The_Weather/The_Weather/wsgi.py
@@ -13,4 +13,4 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'The_Weather.settings')
 
-application = get_wsgi_application()
+app = get_wsgi_application()
